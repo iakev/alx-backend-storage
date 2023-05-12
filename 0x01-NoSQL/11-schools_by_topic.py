b59e8f735cb7acd483c4d27760fdc676bfd083c3
iakev <kirimikmwongera@gmail.com>
2023-05-12 04:38:21 -0700

    Added a function that performs a query on a list in mongo

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pyton3
+"""
+contains a function that returns list of schools having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns lists of schools having a topic
+    """
+    return list(mongo_collection.find({"topics": topic}))
