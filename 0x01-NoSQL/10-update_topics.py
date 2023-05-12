45206a037b9ed112461cda95776afac82c47805b
iakev <kirimikmwongera@gmail.com>
2023-05-12 03:21:11 -0700

    Added a function that updates a collection document accordingly

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Containd a function that updates a document in a collection
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    """
+    mongo_collection.find_one_and_update(
+        {'name': name}, {'$set': {'topics': topics}})
