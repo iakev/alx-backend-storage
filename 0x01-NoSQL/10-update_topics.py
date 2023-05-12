5af711dfbf38ce22c8db4320e6fd481380e29752
iakev <kirimikmwongera@gmail.com>
2023-05-12 03:39:13 -0700

    Modified to set upsert option

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,4 +9,4 @@ def update_topics(mongo_collection, name, topics):
     Changes all topics of a school document based on the name
     """
     mongo_collection.find_one_and_update(
-        {'name': name}, {'$set': {'topics': topics}})
+        {'name': name}, {'$set': {'topics': topics}}, upsert=True)
